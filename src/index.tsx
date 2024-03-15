9c55bacf3aac28b92a0bd6e1c3fdc26c6cd8cbce
Paul <paul-nicolaegeambazu@Paul-2.local>
2024-03-15 09:34:39 +0000

    refractor

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import App from './App'
 import reportWebVitals from './reportWebVitals'
 import { QueryClient, QueryClientProvider } from 'react-query'
 
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 const queryClient = new QueryClient()
 
