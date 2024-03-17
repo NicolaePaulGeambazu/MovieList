384bd625ed500b821547286c00f20ce166f7eec3
Paul <paul-nicolaegeambazu@Paul-2.local>
2024-03-17 21:54:13 +0000

    add hover to card

--- src/components/MovieDetails/MovieCard.style.tsx
+++ src/components/MovieDetails/MovieCard.style.tsx
@@ -1,5 +1,5 @@
-import styled from 'styled-components'
-import { IoStarOutline } from 'react-icons/io5'
+import styled from 'styled-components';
+import { IoStarOutline } from 'react-icons/io5';
 
 export const CardContainer = styled.div`
   width: 300px;
@@ -8,19 +8,26 @@ export const CardContainer = styled.div`
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
   margin: auto;
   position: relative;
-`
+`;
 
 export const CardImage = styled.img`
   width: 100%;
   height: auto;
   border-top-left-radius: 10px;
   border-top-right-radius: 10px;
-`
+  transition: transform 0.2s ease;
+
+  &:hover {
+    transform: scale(1.05);
+  }
+`;
 
 export const ButonFav = styled(IoStarOutline)`
   font-size: 24px;
   color: #c4c4c4;
-`
+  transition: transform 0.2s ease;
+  margin-top: 10px;
+`;
 
 export const IconContainer = styled.div`
   position: absolute;
@@ -29,4 +36,10 @@ export const IconContainer = styled.div`
   z-index: 1;
   background: #f0f33c;
   height: 45px;
-`
+  transition: transform 0.2s ease;
+
+  ${CardContainer}:hover & {
+    transform: translate(0, -30%);
+  }
+`;
+
